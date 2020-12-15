90f51bb5bdab8f96cd4186db2949ff7950d71455
leonardo15043 <lahernandez15043@gmail.com>
2020-12-15 15:37:34 -0500

    Validar que el parrafo tenga la propiedad titulo que le pasamos al componente GitGridItem y validar que existe la clase animate__fadeInLeft

--- src/test/GitGridItem.test.js
+++ src/test/GitGridItem.test.js
@@ -2,17 +2,35 @@ import { shallow } from "enzyme"
 import { GitGridItem } from "../components/GitGridItem"
 
 describe('Pruebas en GitGridItem', () => {
+
+    const image = { 
+        id: 12,
+        title: 'Bored One',
+        url : 'https://media4.giphy.com/media/6IkjQmpaRwIabJ2G3C/giphy-downsized-medium.gif?cid=cd622c90mqvu6s36rmyko0ies0n0xgn9t26d0wqtlmkvdpzw&rid=giphy-downsized-medium.gif'
+    }
+
+
+    const wrapper = shallow(<GitGridItem { ...image } />);
+
     test('Cargar correctamente el componente', () => {
 
-        const image = { 
-            id: 12,
-            title: 'Bored One',
-            url : 'https://media4.giphy.com/media/6IkjQmpaRwIabJ2G3C/giphy-downsized-medium.gif?cid=cd622c90mqvu6s36rmyko0ies0n0xgn9t26d0wqtlmkvdpzw&rid=giphy-downsized-medium.gif'
-        }
-
-        const wapper = shallow(<GitGridItem { ...image } />);
-
-        expect( wapper ).toMatchSnapshot();
+        expect( wrapper ).toMatchSnapshot();
         
     })  
-})
+
+    test('Validar que el parrafo tenga el titulo que le enviamos como parametro ', () => {
+        
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe( image.title );
+
+    })
+
+    test('debe tener la clase animate__fadeInLeft', () => {
+        
+        const div = wrapper.find('div');
+        expect(div.exists('.animate__fadeInLeft')).toBe( true );
+        
+    })
+    
+    
+}) 
